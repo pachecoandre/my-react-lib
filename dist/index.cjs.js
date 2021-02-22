b301355311dc12c92b462ab530c7ac99375fbc32
Andre <andre.henrique@ahfin.com.br>
2021-02-22 15:07:17 -0300

    init

---
+++ dist/index.cjs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+function HelloWorld() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World");
+}
+
+exports.HelloWorld = HelloWorld;
---
+++ playground/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HelloWorld } from 'my-react-lib';
+
+const App = ()  => {
+
+    return (
+        <HelloWorld />
+    );
+}
+
+export default App;
